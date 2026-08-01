c8dc840d92132f8558adfda6addc37d819d96059
Ayush Verma <ayushverma11thb@gmail.com>
2026-08-01 12:28:38 +0530

    Time: 3 ms (99.85%) | Memory: 45.9 MB (97.73%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,14 +1,10 @@
 class Solution {
     public String intToRoman(int num) {
         int[] val = {
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
+            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,1
         };
         String[] syms = {
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
+            "M", "CM", "D", "CD", "C", "XC", "L", "XL",
             "X", "IX", "V", "IV",
             "I"
         };
