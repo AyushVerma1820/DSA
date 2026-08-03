ee756b71842e720446695465f53c9be5599c6b55
Ayush Verma <ayushverma11thb@gmail.com>
2026-08-03 19:58:39 +0530

    Time: 2 ms (46.74%) | Memory: 43.4 MB (69.80%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,8 +1,25 @@
-import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger x = new BigInteger(a, 2);
-        BigInteger y = new BigInteger(b, 2);
-        return x.add(y).toString(2);
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int total = carry;
+            if (i >= 0) {
+                total += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                total += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(total % 2);
+            carry = total / 2;
+        }
+
+        return result.reverse().toString();
     }
 }
