923ee915fe8751b1c98b332cb797e42146c0dffd
Ayush Verma <ayushverma11thb@gmail.com>
2026-08-10 12:20:38 +0530

    Time: 21 ms (41.65%) | Memory: 45.4 MB (95.16%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,45 +1,31 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        if (n < 4) return result;
-
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < n - 3; i++) {
-            // skip duplicate values for i
+        for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            // early termination / pruning
-            if ((long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
-            if ((long) nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target) continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
-                // skip duplicate values for j
+            for (int j = i + 1; j < nums.length - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                if ((long) nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
-                if ((long) nums[i] + nums[j] + nums[n-1] + nums[n-2] < target) continue;
 
-                int left = j + 1, right = n - 1;
-                long remaining = (long) target - nums[i] - nums[j];
+                int l = j + 1, r = nums.length - 1;
 
-                while (left < right) {
-                    long sum = nums[left] + nums[right];
-                    if (sum == remaining) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left++;
-                        right--;
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-                        while (left < right && nums[right] == nums[right + 1]) right--;
-                    } else if (sum < remaining) {
-                        left++;
+                while (l < r) {
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+
+                    if (sum == target) {
+                        ans.add(List.of(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[++l]);
+                        while (l < r && nums[r] == nums[--r]);
+                    } else if (sum < target) {
+                        l++;
                     } else {
-                        right--;
+                        r--;
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 }
